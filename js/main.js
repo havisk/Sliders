8fe1b5cb6787061a5930bcc148593093a7e28e09
Kool Havis <havisk@gmail.com>
2015-10-07 02:37:17 -0400

    finished

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// click on headline link
+	$('.test').on('click', function (event){
+	$('#up').removeClass('close').addClass('open');
+	$('#set').removeClass('open').addClass('close');
+
+});
+	$('.more').on('click', function (event){
+	$('#up').removeClass('open').addClass('close');
+	$('#set').removeClass('close').addClass('open');
+});
+
+	$('.illini').on('click', function (event){
+	$('#up').removeClass('open').addClass('close');
+	$('#set').removeClass('open').addClass('close');
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Using CSS transitions to open and close sections.
